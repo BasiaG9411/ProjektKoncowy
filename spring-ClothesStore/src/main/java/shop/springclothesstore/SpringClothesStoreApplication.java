cf8658c87bac3f1e18cb8c726c08cc49da8e4982
Basia <barbara.gryciuk1@gmail.com>
2023-05-18 17:46:39 +0200

    1. Start application

---
+++ spring-ClothesStore/src/main/java/shop/springclothesstore/SpringClothesStoreApplication.java
@@ -0,0 +1,14 @@
+package shop.springclothesstore;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringClothesStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringClothesStoreApplication.class, args);
+	}
+
+}
---
+++ spring-ClothesStore/src/test/java/shop/springclothesstore/SpringClothesStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.springclothesstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringClothesStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
