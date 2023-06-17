874321370858f9a6bca3c5c414e66606fe419020
Basia <barbara.gryciuk1@gmail.com>
2023-06-17 15:02:28 +0200

    add logos

--- spring-ClothesStore/src/main/java/shop/springclothesstore/repository/ProductRepository.java
+++ spring-ClothesStore/src/main/java/shop/springclothesstore/repository/ProductRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import shop.springclothesstore.entity.Product;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product,Long> {
+    List<Product> findProductsByIdIn(List<Long> ids);
 }
